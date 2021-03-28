1dd2eb81521ec0bb7f5c44288cdbf31b6ff89021
Katona Áron <katonaaron01@gmail.com>
2021-03-28 13:05:17 +0300

    Add further experiments

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -10,27 +10,122 @@ void cameraCalibration()
 	TermCriteria criteria = TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 30, 0.1);
 	Size boardSize = Size(7, 6);
 
+	std::vector<std::vector<Point2f>> imgPoints;
+	std::vector<std::vector<Point3f>> objectPoints;
+	std::vector<Point3f> objp;
+	std::vector<Mat>images;
+
+
+	for (int i = 0; i < boardSize.width; i++) {
+		for (int j = 0; j < boardSize.height; j++) {
+			objp.push_back(Point3f(i, j, 0));
+		}
+	}
+
+
 	char folderName[MAX_PATH];
 	if (openFolderDlg(folderName) == 0)
 		return;
+
+
+	
+
 	char fname[MAX_PATH];
 	FileGetter fg(folderName, "jpg");
 	while (fg.getNextAbsFile(fname))
 	{
+		std::cout << fname << "\n";
+
 		Mat_<uchar> src = imread(fname, IMREAD_GRAYSCALE);
 		std::vector<Point2f> pointBuf;
 		bool found = findChessboardCorners(src, boardSize, pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK);
 
-		if(found)
+		if (found)
 		{
 			cornerSubPix(src, pointBuf, Size(11, 11), Size(-1, -1), criteria);
 			drawChessboardCorners(src, boardSize, Mat(pointBuf), found);
+			imgPoints.push_back(pointBuf);
+			images.push_back(src);
 		}
-		
-		imshow(fg.getFoundFileName(), src);
-		if (waitKey() == 27) //ESC pressed
-			break;
+
+
+
+		//imshow(fg.getFoundFileName(), src);
+		//if (waitKey() == 27) //ESC pressed
+		//	break;
 	}
+
+	objectPoints.resize(imgPoints.size(), objp);
+
+	
+
+
+	for(const auto& img : images)
+	{
+
+		Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+		Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
+		std::vector<Mat> rotVec;
+		std::vector<Mat> transVec;
+
+
+		Size imageSize = img.size();
+
+		double rms = calibrateCamera(objectPoints, imgPoints, imageSize, cameraMatrix, distCoeffs, rotVec, transVec, 0);
+
+		std::cout << "rms: " << rms << "\n";
+		std::cout << "cameraMatrix:\n" << cameraMatrix << "\n";
+		std::cout << "distCoeffs:\n" << distCoeffs << "\n";
+		std::cout << "rotVec:\n";
+		for (const auto& elem : rotVec) {
+			std::cout << elem << "\n";
+		}
+		std::cout << "\n";
+		std::cout << "transVec:\n";
+		for (const auto& elem : transVec) {
+			std::cout << elem << "\n";
+		}
+		std::cout << "\n";
+
+		Mat newCameraMatrix = getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imageSize, 1, imageSize, 0);
+
+
+		Mat dst = img.clone();
+		//undistort(img, dst, newCameraMatrix, distCoeffs);
+
+
+		Mat map1, map2;
+
+		initUndistortRectifyMap(
+			cameraMatrix, distCoeffs, Mat(),
+			getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imageSize, 1, imageSize, 0), imageSize,
+			CV_16SC2, map1, map2);
+		remap(img, dst, map1, map2, INTER_LINEAR);
+
+
+		imshow("input image", img);
+		imshow("resulted image", dst);
+		waitKey();
+	}
+
+
+	
+
+
+
+
+	//initUndistortRectifyMap(
+	//	cameraMatrix, distCoeffs, Mat(),
+	//	, imageSize,
+	//	CV_16SC2, map1, map2);
+
+
+	//Mat view = src.clone();
+
+	//remap(view, rview, map1, map2, INTER_LINEAR);
+	//imshow("Image View", rview);
+
+	//waitKey();
 }
 
 void testCameraCalibration()
@@ -41,11 +136,11 @@ void testCameraCalibration()
 void testOpenImage()
 {
 	char fname[MAX_PATH];
-	while(openFileDlg(fname))
+	while (openFileDlg(fname))
 	{
 		Mat src;
 		src = imread(fname);
-		imshow("image",src);
+		imshow("image", src);
 		waitKey();
 	}
 }
@@ -53,16 +148,16 @@ void testOpenImage()
 void testOpenImagesFld()
 {
 	char folderName[MAX_PATH];
-	if (openFolderDlg(folderName)==0)
+	if (openFolderDlg(folderName) == 0)
 		return;
 	char fname[MAX_PATH];
-	FileGetter fg(folderName,"jpg");
-	while(fg.getNextAbsFile(fname))
+	FileGetter fg(folderName, "jpg");
+	while (fg.getNextAbsFile(fname))
 	{
 		Mat src;
 		src = imread(fname);
-		imshow(fg.getFoundFileName(),src);
-		if (waitKey()==27) //ESC pressed
+		imshow(fg.getFoundFileName(), src);
+		if (waitKey() == 27) //ESC pressed
 			break;
 	}
 }
@@ -105,23 +200,23 @@ void testImageOpenAndSave()
 void testNegativeImage()
 {
 	char fname[MAX_PATH];
-	while(openFileDlg(fname))
+	while (openFileDlg(fname))
 	{
 		double t = (double)getTickCount(); // Get the current time [s]
-		
-		Mat src = imread(fname,IMREAD_GRAYSCALE);
+
+		Mat src = imread(fname, IMREAD_GRAYSCALE);
 		int height = src.rows;
 		int width = src.cols;
-		Mat dst = Mat(height,width,CV_8UC1);
+		Mat dst = Mat(height, width, CV_8UC1);
 		// Asa se acceseaaza pixelii individuali pt. o imagine cu 8 biti/pixel
 		// Varianta ineficienta (lenta)
-		for (int i=0; i<height; i++)
+		for (int i = 0; i < height; i++)
 		{
-			for (int j=0; j<width; j++)
+			for (int j = 0; j < width; j++)
 			{
-				uchar val = src.at<uchar>(i,j);
+				uchar val = src.at<uchar>(i, j);
 				uchar neg = 255 - val;
-				dst.at<uchar>(i,j) = neg;
+				dst.at<uchar>(i, j) = neg;
 			}
 		}
 
@@ -130,8 +225,8 @@ void testNegativeImage()
 		// Print (in the console window) the processing time in [ms] 
 		printf("Time = %.3f [ms]\n", t * 1000);
 
-		imshow("input image",src);
-		imshow("negative image",dst);
+		imshow("input image", src);
+		imshow("negative image", dst);
 		waitKey();
 	}
 }
@@ -149,13 +244,13 @@ void testParcurgereSimplaDiblookStyle()
 		double t = (double)getTickCount(); // Get the current time [s]
 
 		// the fastest approach using the “diblook style”
-		uchar *lpSrc = src.data;
-		uchar *lpDst = dst.data;
-		int w = (int) src.step; // no dword alignment is done !!!
-		for (int i = 0; i<height; i++)
+		uchar* lpSrc = src.data;
+		uchar* lpDst = dst.data;
+		int w = (int)src.step; // no dword alignment is done !!!
+		for (int i = 0; i < height; i++)
 			for (int j = 0; j < width; j++) {
-				uchar val = lpSrc[i*w + j];
-				lpDst[i*w + j] = 255 - val;
+				uchar val = lpSrc[i * w + j];
+				lpDst[i * w + j] = 255 - val;
 			}
 
 		// Get the current time again and compute the time difference [s]
@@ -163,8 +258,8 @@ void testParcurgereSimplaDiblookStyle()
 		// Print (in the console window) the processing time in [ms] 
 		printf("Time = %.3f [ms]\n", t * 1000);
 
-		imshow("input image",src);
-		imshow("negative image",dst);
+		imshow("input image", src);
+		imshow("negative image", dst);
 		waitKey();
 	}
 }
@@ -172,31 +267,31 @@ void testParcurgereSimplaDiblookStyle()
 void testColor2Gray()
 {
 	char fname[MAX_PATH];
-	while(openFileDlg(fname))
+	while (openFileDlg(fname))
 	{
 		Mat src = imread(fname);
 
 		int height = src.rows;
 		int width = src.cols;
 
-		Mat dst = Mat(height,width,CV_8UC1);
+		Mat dst = Mat(height, width, CV_8UC1);
 
 		// Asa se acceseaaza pixelii individuali pt. o imagine RGB 24 biti/pixel
 		// Varianta ineficienta (lenta)
-		for (int i=0; i<height; i++)
+		for (int i = 0; i < height; i++)
 		{
-			for (int j=0; j<width; j++)
+			for (int j = 0; j < width; j++)
 			{
-				Vec3b v3 = src.at<Vec3b>(i,j);
+				Vec3b v3 = src.at<Vec3b>(i, j);
 				uchar b = v3[0];
 				uchar g = v3[1];
 				uchar r = v3[2];
-				dst.at<uchar>(i,j) = (r+g+b)/3;
+				dst.at<uchar>(i, j) = (r + g + b) / 3;
 			}
 		}
-		
-		imshow("input image",src);
-		imshow("gray image",dst);
+
+		imshow("input image", src);
+		imshow("gray image", dst);
 		waitKey();
 	}
 }
@@ -226,12 +321,12 @@ void testBGR2HSV()
 		// definire pointer la matricea (24 biti/pixeli) a imaginii HSV
 		uchar* hsvDataPtr = hsvImg.data;
 
-		for (int i = 0; i<height; i++)
+		for (int i = 0; i < height; i++)
 		{
-			for (int j = 0; j<width; j++)
+			for (int j = 0; j < width; j++)
 			{
-				int hi = i*width * 3 + j * 3;
-				int gi = i*width + j;
+				int hi = i * width * 3 + j * 3;
+				int gi = i * width + j;
 
 				lpH[gi] = hsvDataPtr[hi] * 510 / 360;		// lpH = 0 .. 255
 				lpS[gi] = hsvDataPtr[hi + 1];			// lpS = 0 .. 255
@@ -251,18 +346,18 @@ void testBGR2HSV()
 void testResize()
 {
 	char fname[MAX_PATH];
-	while(openFileDlg(fname))
+	while (openFileDlg(fname))
 	{
 		Mat src;
 		src = imread(fname);
-		Mat dst1,dst2;
+		Mat dst1, dst2;
 		//without interpolation
-		resizeImg(src,dst1,320,false);
+		resizeImg(src, dst1, 320, false);
 		//with interpolation
-		resizeImg(src,dst2,320,true);
-		imshow("input image",src);
-		imshow("resized image (without interpolation)",dst1);
-		imshow("resized image (with interpolation)",dst2);
+		resizeImg(src, dst2, 320, true);
+		imshow("input image", src);
+		imshow("resized image (without interpolation)", dst1);
+		imshow("resized image (with interpolation)", dst2);
 		waitKey();
 	}
 }
@@ -270,17 +365,17 @@ void testResize()
 void testCanny()
 {
 	char fname[MAX_PATH];
-	while(openFileDlg(fname))
+	while (openFileDlg(fname))
 	{
-		Mat src,dst,gauss;
-		src = imread(fname,IMREAD_GRAYSCALE);
+		Mat src, dst, gauss;
+		src = imread(fname, IMREAD_GRAYSCALE);
 		double k = 0.4;
 		int pH = 50;
-		int pL = (int) k*pH;
+		int pL = (int)k * pH;
 		GaussianBlur(src, gauss, Size(5, 5), 0.8, 0.8);
-		Canny(gauss,dst,pL,pH,3);
-		imshow("input image",src);
-		imshow("canny",dst);
+		Canny(gauss, dst, pL, pH, 3);
+		imshow("input image", src);
+		imshow("canny", dst);
 		waitKey();
 	}
 }
@@ -294,7 +389,7 @@ void testVideoSequence()
 		waitKey(0);
 		return;
 	}
-		
+
 	Mat edges;
 	Mat frame;
 	char c;
@@ -303,14 +398,14 @@ void testVideoSequence()
 	{
 		Mat grayFrame;
 		cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
-		Canny(grayFrame,edges,40,100,3);
+		Canny(grayFrame, edges, 40, 100, 3);
 		imshow("source", frame);
 		imshow("gray", grayFrame);
 		imshow("edges", edges);
 		c = waitKey(0);  // waits a key press to advance to the next frame
 		if (c == 27) {
 			// press ESC to exit
-			printf("ESC pressed - capture finished\n"); 
+			printf("ESC pressed - capture finished\n");
 			break;  //ESC pressed
 		};
 	}
@@ -329,7 +424,7 @@ void testSnap()
 	Mat frame;
 	char numberStr[256];
 	char fileName[256];
-	
+
 	// video resolution
 	Size capS = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
 		(int)cap.get(CAP_PROP_FRAME_HEIGHT));
@@ -357,7 +452,7 @@ void testSnap()
 		}
 
 		++frameNum;
-		
+
 		imshow(WIN_SRC, frame);
 
 		c = waitKey(10);  // waits a key press to advance to the next frame
@@ -366,7 +461,7 @@ void testSnap()
 			printf("ESC pressed - capture finished");
 			break;  //ESC pressed
 		}
-		if (c == 115){ //'s' pressed - snapp the image to a file
+		if (c == 115) { //'s' pressed - snapp the image to a file
 			frameCount++;
 			fileName[0] = NULL;
 			sprintf(numberStr, "%d", frameCount);
@@ -374,7 +469,7 @@ void testSnap()
 			strcat(fileName, numberStr);
 			strcat(fileName, ".bmp");
 			bool bSuccess = imwrite(fileName, frame);
-			if (!bSuccess) 
+			if (!bSuccess)
 			{
 				printf("Error writing the snapped image\n");
 			}
@@ -390,13 +485,13 @@ void MyCallBackFunc(int event, int x, int y, int flags, void* param)
 	//More examples: http://opencvexamples.blogspot.com/2014/01/detect-mouse-clicks-and-moves-on-image.html
 	Mat* src = (Mat*)param;
 	if (event == EVENT_LBUTTONDOWN)
-		{
-			printf("Pos(x,y): %d,%d  Color(RGB): %d,%d,%d\n",
-				x, y,
-				(int)(*src).at<Vec3b>(y, x)[2],
-				(int)(*src).at<Vec3b>(y, x)[1],
-				(int)(*src).at<Vec3b>(y, x)[0]);
-		}
+	{
+		printf("Pos(x,y): %d,%d  Color(RGB): %d,%d,%d\n",
+			x, y,
+			(int)(*src).at<Vec3b>(y, x)[2],
+			(int)(*src).at<Vec3b>(y, x)[1],
+			(int)(*src).at<Vec3b>(y, x)[0]);
+	}
 }
 
 void testMouseClick()
@@ -436,9 +531,9 @@ void showHistogram(const std::string& name, int* hist, const int  hist_cols, con
 
 	//computes histogram maximum
 	int max_hist = 0;
-	for (int i = 0; i<hist_cols; i++)
-	if (hist[i] > max_hist)
-		max_hist = hist[i];
+	for (int i = 0; i < hist_cols; i++)
+		if (hist[i] > max_hist)
+			max_hist = hist[i];
 	double scale = 1.0;
 	scale = (double)hist_height / max_hist;
 	int baseline = hist_height - 1;
@@ -471,45 +566,44 @@ int main()
 		printf(" 9 - Mouse callback demo\n");
 
 		printf("\n 10 - Project Camera Calibration\n");
-		
+
 		printf(" 0 - Exit\n\n");
 		printf("Option: ");
-		scanf("%d",&op);
+		scanf("%d", &op);
 		switch (op)
 		{
-			case 1:
-				testOpenImage();
-				break;
-			case 2:
-				testOpenImagesFld();
-				break;
-			case 3:
-				testParcurgereSimplaDiblookStyle(); //diblook style
-				break;
-			case 4:
-				//testColor2Gray();
-				testBGR2HSV();
-				break;
-			case 5:
-				testResize();
-				break;
-			case 6:
-				testCanny();
-				break;
-			case 7:
-				testVideoSequence();
-				break;
-			case 8:
-				testSnap();
-				break;
-			case 9:
-				testMouseClick();
-				break;
-			case 10:
-				testCameraCalibration();
-				break;
+		case 1:
+			testOpenImage();
+			break;
+		case 2:
+			testOpenImagesFld();
+			break;
+		case 3:
+			testParcurgereSimplaDiblookStyle(); //diblook style
+			break;
+		case 4:
+			//testColor2Gray();
+			testBGR2HSV();
+			break;
+		case 5:
+			testResize();
+			break;
+		case 6:
+			testCanny();
+			break;
+		case 7:
+			testVideoSequence();
+			break;
+		case 8:
+			testSnap();
+			break;
+		case 9:
+			testMouseClick();
+			break;
+		case 10:
+			testCameraCalibration();
+			break;
 		}
-	}
-	while (op!=0);
+	} while (op != 0);
 	return 0;
 }
